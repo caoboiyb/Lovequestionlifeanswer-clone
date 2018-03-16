bf60ad1eb3357abaa229fcd89aaf677ff2989a71
trungducng <caoboiyb1997@gmail.com>
2018-03-16 18:06:16 +0700

    LQLA

---
+++ Lovequestionlifeanswer-Clone/Controller/CategoryVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Lovequestionlifeanswer-Clone
+//
+//  Created by Trung Duc on 3/16/18.
+//  Copyright © 2018 Trung Duc. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
